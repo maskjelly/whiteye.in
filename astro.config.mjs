f7092e45b2b17fdf2a279e11c7eede01c3d6c161
asdfghjA1 <aryan14decsingh@gmail.com>
2023-08-02 15:13:04 +0530

    bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import {loglib} from "@loglib/tracker"
-
 
 // https://astro.build/config
 
