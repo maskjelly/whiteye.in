2bd25bd5254cf1c62b6af88c03eec6140a857c31
asdfghjA1 <aryan14decsingh@gmail.com>
2023-08-02 13:22:31 +0530

    Analytics Added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,16 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
+
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
   site: SITE.website,
   integrations: [
     tailwind({
