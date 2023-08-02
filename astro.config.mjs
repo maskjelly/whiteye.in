720538bfd39e75de002b7025079f5e3c334e3d3d
asdfghjA1 <aryan14decsingh@gmail.com>
2023-08-02 15:57:37 +0530

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/edge';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
@@ -10,40 +9,35 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    analytics: true,
-  }),
-
-  // site: SITE.website,
-  // integrations: [
-  //   tailwind({
-  //     config: {
-  //       applyBaseStyles: false,
-  //     },
-  //   }),
-  //   react(),
-  //   sitemap(),
-  // ],
-  // markdown: {
-  //   remarkPlugins: [
-  //     remarkToc,
-  //     [
-  //       remarkCollapse,
-  //       {
-  //         test: "Table of contents",
-  //       },
-  //     ],
-  //   ],
-  //   shikiConfig: {
-  //     theme: "one-dark-pro",
-  //     wrap: true,
-  //   },
-  //   extendDefaultPlugins: true,
-  // },
-  // vite: {
-  //   optimizeDeps: {
-  //     exclude: ["@resvg/resvg-js"],
-  //   },
-  // },
+  site: SITE.website,
+  integrations: [
+    tailwind({
+      config: {
+        applyBaseStyles: false,
+      },
+    }),
+    react(),
+    sitemap(),
+  ],
+  markdown: {
+    remarkPlugins: [
+      remarkToc,
+      [
+        remarkCollapse,
+        {
+          test: "Table of contents",
+        },
+      ],
+    ],
+    shikiConfig: {
+      theme: "one-dark-pro",
+      wrap: true,
+    },
+    extendDefaultPlugins: true,
+  },
+  vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
