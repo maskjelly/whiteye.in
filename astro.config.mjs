ffde208e37ef9440c5e3e681c7e93c8c6c522e48
asdfghjA1 <aryan14decsingh@gmail.com>
2023-08-02 15:24:20 +0530

    Vercel try 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import vercel from '@astrojs/vercel/edge';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import remarkToc from "remark-toc";
@@ -9,7 +10,10 @@ import { SITE } from "./src/config";
 // https://astro.build/config
 
 export default defineConfig({
-
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
 
   site: SITE.website,
   integrations: [
