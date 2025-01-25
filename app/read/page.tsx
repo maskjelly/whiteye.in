720efab846cc7d87c5524431509e963ec2928f25
asdfghjA1 <aaryan14decembersingh@gmail.com>
2025-01-25 05:53:55 +0530

    new reading page im working to help new people get into ai and infra development

---
+++ app/read/page.tsx
@@ -0,0 +1,23 @@
+import { link } from "fs";
+
+export default function READ(){
+    return (
+        <div>
+            <li>
+                Attention is all you need
+            </li>
+            <li>
+                GPT3 Research Paper
+            </li>
+            <li>
+                LLAMA3 PAPERS
+            </li>
+            <li>
+
+            </li>
+            <li>
+                DEEPSEEK R1 Paper
+            </li>
+        </div>
+    )
+}
