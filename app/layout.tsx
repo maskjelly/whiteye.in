c988d3422199a651ec7e90bf50534d75c44702b2
maskjelly <aaryan14decembersingh@gmail.com>
2025-04-27 16:02:07 +0530

    new bg for server uses , theif

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
